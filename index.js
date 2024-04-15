7d75054efe848dbc74648401ff6a2a5875dca561
RishiKnorket <151186775+RishiKnorket@users.noreply.github.com>
2024-04-15 13:10:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,118 @@
+const jsonData = {
+  "knorket": {
+    "auth": {},
+    "information_schema": {
+      "applicable_roles": [
+        ["grantee", "INT"],
+        ["grantee_type", "int"],
+        ["role_name", "VARCHAR"],
+        ["is_grantable", "varchar"],
+        ["is_grantable", "DATE"],
+        ["is_grantable", "sadadss"],
+      ],
+      "columns": [
+        ["table_catalog", "varchar"],
+        ["table_schema", "varchar"],
+        ["table_name", "varchar"],
+        ["column_name", "varchar"],
+        ["ordinal_position", "bigint"],
+        ["column_default", "varchar"],
+        ["is_nullable", "varchar"],
+        ["data_type", "varchar"]
+      ],
+      "enabled_roles": [
+        ["role_name", "varchar"]
+      ],
+      "roles": [
+        ["role_name", "varchar"]
+      ],
+      "schemata": [
+        ["catalog_name", "varchar"],
+        ["schema_name", "varchar"]
+      ],
+      "table_privileges": [
+        ["grantor", "varchar"],
+        ["grantor_type", "varchar"],
+        ["grantee", "varchar"],
+        ["grantee_type", "varchar"],
+        ["table_catalog", "varchar"],
+        ["table_schema", "varchar"],
+        ["table_name", "varchar"],
+        ["privilege_type", "varchar"],
+        ["is_grantable", "varchar"],
+        ["with_hierarchy", "varchar"]
+      ],
+      "tables": [
+        ["table_catalog", "varchar"],
+        ["table_schema", "varchar"],
+        ["table_name", "varchar"],
+        ["table_type", "varchar"]
+      ],
+      "views": [
+        ["table_catalog", "varchar"],
+        ["table_schema", "varchar"],
+        ["table_name", "varchar"],
+        ["view_definition", "varchar"]
+      ]
+    },
+    "vault": {}
+  },
+  "connect": {
+    "Public": {},
+    "information_schema": {
+      "applicable_roles": [
+        ["grantee", "varchar"],
+        ["grantee_type", "varchar"],
+        ["role_name", "varchar"],
+        ["is_grantable", "varchar"]
+      ],
+      "columns": [
+        ["table_catalog", "varchar"],
+        ["table_schema", "varchar"],
+        ["table_name", "varchar"],
+        ["column_name", "varchar"],
+        ["ordinal_position", "bigint"],
+        ["column_default", "varchar"],
+        ["is_nullable", "varchar"],
+        ["data_type", "varchar"]
+      ],
+      "enabled_roles": [
+        ["role_name", "varchar"]
+      ],
+      "roles": [
+        ["role_name", "varchar"]
+      ],
+      "schemata": [
+        ["catalog_name", "varchar"],
+        ["schema_name", "varchar"]
+      ],
+      "table_privileges": [
+        ["grantor", "varchar"],
+        ["grantor_type", "varchar"],
+        ["grantee", "varchar"],
+        ["grantee_type", "varchar"],
+        ["table_catalog", "varchar"],
+        ["table_schema", "varchar"],
+        ["table_name", "varchar"],
+        ["privilege_type", "varchar"],
+        ["is_grantable", "varchar"],
+        ["with_hierarchy", "varchar"]
+      ],
+      "tables": [
+        ["table_catalog", "varchar"],
+        ["table_schema", "varchar"],
+        ["table_name", "varchar"],
+        ["table_type", "varchar"]
+      ],
+      "views": [
+        ["table_catalog", "varchar"],
+        ["table_schema", "varchar"],
+        ["table_name", "varchar"],
+        ["view_definition", "varchar"]
+      ]
+    },
+    "vault": {}
+
+  }
+}
+
